41401b1c36e5ae312e315875c5cf9ff694ca9dac
Rmckays <rmckays@gmail.com>
2023-02-21 08:22:59 -0700

    Initial commit.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInput : MonoBehaviour
+{
+    [field: SerializeField]
+    public Vector2 MovementVector { get; private set; }
+
+    public event Action OnAttack, OnJumpPressed, OnJumpReleased, OnWeaponChange;
+
+    public event Action<Vector2> OnMovement;
+
+    public KeyCode jumpKey, attackKey, menuKey;
+
+    public UnityEvent OnMenuKeyPressed;
+}
