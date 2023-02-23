8ecb7d793a6dd5ad3d5d1448ac6b2fbe4821178d
Rmckays <rmckays@gmail.com>
2023-02-23 08:37:11 -0700

    Add player animation.

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -7,11 +7,13 @@ public class Agent : MonoBehaviour
 {
     public Rigidbody2D rigidBody2D;
     public PlayerInput playerInput;
+    public AgentAnimation agentAnimation;
 
     private void Awake()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
         playerInput = GetComponentInParent<PlayerInput>();
+        agentAnimation = GetComponentInChildren<AgentAnimation>();
     }
 
     private void Start()
@@ -23,11 +25,21 @@ public class Agent : MonoBehaviour
     {
         if (Math.Abs(input.x) > 0)
         {
+            if (Math.Abs(rigidBody2D.velocity.x) < 0.01f)
+            {
+                agentAnimation.PlayAnimation(AnimationType.run);
+            }
             rigidBody2D.velocity = new Vector2(input.x * 5, rigidBody2D.velocity.y);
         }
         else
         {
+            if (Math.Abs(rigidBody2D.velocity.x) > 0)
+            {
+                agentAnimation.PlayAnimation(AnimationType.idle);
+            }
             rigidBody2D.velocity = new Vector2(0, rigidBody2D.velocity.y);
         }
     }
+    
+    
 }
